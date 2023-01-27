45c8b479faa4c6bdd31387c1d30bc9aef0a9cf15
Philipp Sanktjohanser <venture@mein.gmx>
2023-01-27 06:18:58 +0100

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Philipp Sanktjohanser on 27.01.23.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
